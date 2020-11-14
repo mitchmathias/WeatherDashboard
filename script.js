83215a662f3d1132af810eb1c3665ac9de8400ba
Mitch <mitch.mathias96@gmail.com>
2020-11-13 19:02:53 -0500

    updated js for 5 day forcast

--- script.js
+++ script.js
@@ -10,35 +10,83 @@ $("#searchButton").on("click", function (event) {
     var city = $("#citySearch").val().trim()
     // Here we are building the URL we need to query the database
     var dailyQueryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&units=imperial&cnt=6&appid=" + APIKey;
-// current day
+    // current day
     $.ajax({
         url: dailyQueryURL,
         method: "GET",
+
         success: function (data) {
-            
+
+            // get humidity, uv index, wind, next 5 days, etc.
             var tempToday = data.list[0].temp.day;
             var city = data.city.name
             var humidityToday = data.list[0].humidity
             var windToday = data.list[0].speed
 
-                // get humidity, uv index, wind, next 5 days, etc.
+            // variables for next 5 days
+            var fc1icon = data.list[1].weather[0].icon
+            var fc1temp = data.list[1].temp.day
+            var fc1humidity = data.list[1].humidity
 
-                setCityValues(tempToday, city, humidityToday, windToday)
+            var fc2icon = data.list[2].weather[0].icon
+            var fc2temp = data.list[2].temp.day
+            var fc2humidity = data.list[2].humidity
+
+            var fc3icon = data.list[3].weather[0].icon
+            var fc3temp = data.list[3].temp.day
+            var fc3humidity = data.list[3].humidity
+
+            var fc4icon = data.list[4].weather[0].icon
+            var fc4temp = data.list[4].temp.day
+            var fc4humidity = data.list[4].humidity
+
+            var fc5icon = data.list[5].weather[0].icon
+            var fc5temp = data.list[5].temp.day
+            var fc5humidity = data.list[5].humidity
+            
             // setCityValues(currentTemp, uvVar, windVar, day1tempVar, etc..)
+            setCityValues(tempToday, city, humidityToday, windToday)
+
+            setForcastValues(fc1icon, fc1temp, fc1humidity, 1)
+            setForcastValues(fc2icon, fc2temp, fc2humidity, 2)
+            setForcastValues(fc3icon, fc3temp, fc3humidity, 3)
+            setForcastValues(fc4icon, fc4temp, fc4humidity, 4)
+            setForcastValues(fc5icon, fc5temp, fc5humidity, 5)
+
+
         },
         error: function (xhr, status, error) {
             var err = eval("(" + xhr.responseText + ")");
             // alert(err.Message);
             alert("error :(")
         }
+
     });
-    
+    console.log(fc1icon)
+    cities.push(city)
+    localStorage.setItem(city, 'city')
+    console.log(cities)
+    console.log(dailyQueryURL)
 });
+
+// document.onload(){
+//     localStorage.setItem(cities);
+//     var city = cities[i]
+//     for( i = 0 ;i < cities.length; i++) {
+
+//     } 
+// }
 
 function setCityValues(tempToday, city, humidityToday, windToday) {
     $("#city").text(city)
     $("#temp").text("Temperature: " + tempToday + " °F")
     $("#humidity").text("Humidity: " + humidityToday + "%")
-    $("#wind").text("wind Speed: " + windToday + "MPH")
+    $("#wind").text("Wind Speed: " + windToday + "MPH")
 }
 
+function setForcastValues(icon, temp, humidity, day){
+    $("#day"+day+"Icon").text("=", icon)
+    $("#day"+day+"Temp").text(temp)
+    $("#day"+day+"Humidity").text(humidity)
+
+}
